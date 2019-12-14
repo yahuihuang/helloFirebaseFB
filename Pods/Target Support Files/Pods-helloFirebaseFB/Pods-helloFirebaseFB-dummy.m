c82e3ebd148aca27337eb0ebb06ff0d6024c001e
Huang Grace <huang.yahui.grace@gmail.com>
2019-12-14 16:09:11 +0800

    FB應用程式設定-part 1

---
+++ Pods/Target Support Files/Pods-helloFirebaseFB/Pods-helloFirebaseFB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_helloFirebaseFB : NSObject
+@end
+@implementation PodsDummy_Pods_helloFirebaseFB
+@end
--- helloFirebaseFB/AppDelegate.swift
+++ helloFirebaseFB/AppDelegate.swift
@@ -7,14 +7,13 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
     }
 
